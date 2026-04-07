0cd34758e6ee349a8a234385e3515eb29449b3b2
Moran-38B <chococa.minecraft@gmail.com>
2026-04-08 02:19:27 +0800

    Add Tailwind styles ot homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,14 @@
 
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://Moran-38B.github.io',
-	base: '/Web.github.io',
+  site: 'https://Moran-38B.github.io',
+  base: '/Web.github.io',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
